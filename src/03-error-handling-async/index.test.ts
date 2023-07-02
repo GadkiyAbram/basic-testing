45641a98b4bc535db3f0cc9e5a3cb29697314410
gadkiyabram <a.s.abramovskiy@tmn3.etagi.com>
2023-07-02 10:51:31 +0500

    Refactored 02-table tests Done 03-error handling

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,56 @@
 // Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
+    const value = 'value exists';
+    const result = resolveValue(value);
+
+    await expect(result).resolves.toBe(value);
     // Write your test here
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
+    const errorMsg = 'Error Message received!';
+
+    expect(() => {
+      throwError(errorMsg);
+    }).toThrowError(errorMsg);
     // Write your test here
   });
 
   test('should throw error with default message if message is not provided', () => {
+    expect(() => {
+      throwError();
+    }).toThrowError('Oops!');
     // Write your test here
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
+    // const result = throwCustomError();
+
+    expect(() => {
+      throwCustomError();
+    }).toThrow(MyAwesomeError);
     // Write your test here
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
+    const result = rejectCustomError();
+
+    await expect(result).rejects.toThrow(MyAwesomeError);
     // Write your test here
   });
 });
